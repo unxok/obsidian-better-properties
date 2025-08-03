418f6371d0df3a63f663c728e0b35c54fd363e7a
unxok <anderluke707@gmail.com>
2025-08-02 22:19:21 -0700

    Added explicit type

--- src2/CustomPropertyTypes/Dropdown/registerListeners.ts
+++ src2/CustomPropertyTypes/Dropdown/registerListeners.ts
@@ -1,18 +1,20 @@
 import { debounce, EventRef } from "obsidian";
 import BetterProperties from "~/main";
 import { refreshPropertyEditor } from "~/MetadataEditor";
+import { CustomPropertyType } from "../types";
 
-export const registerListeners = (plugin: BetterProperties) => {
-	const vaultEventHandler = vaultEventHandlerFactory(plugin);
-	const metadataCacheHandler = metadataCacheEventHandlerFactory(plugin);
-	const eventRefs: EventRef[] = [
-		plugin.app.vault.on("create", vaultEventHandler),
-		plugin.app.vault.on("delete", vaultEventHandler),
-		plugin.app.vault.on("rename", vaultEventHandler),
-		plugin.app.metadataCache.on("changed", metadataCacheHandler),
-	];
-	eventRefs.forEach((ref) => plugin.registerEvent(ref));
-};
+export const registerListeners: CustomPropertyType<string>["registerListeners"] =
+	(plugin: BetterProperties) => {
+		const vaultEventHandler = vaultEventHandlerFactory(plugin);
+		const metadataCacheHandler = metadataCacheEventHandlerFactory(plugin);
+		const eventRefs: EventRef[] = [
+			plugin.app.vault.on("create", vaultEventHandler),
+			plugin.app.vault.on("delete", vaultEventHandler),
+			plugin.app.vault.on("rename", vaultEventHandler),
+			plugin.app.metadataCache.on("changed", metadataCacheHandler),
+		];
+		eventRefs.forEach((ref) => plugin.registerEvent(ref));
+	};
 
 const vaultEventHandlerFactory = (plugin: BetterProperties): (() => void) => {
 	return debounce(
