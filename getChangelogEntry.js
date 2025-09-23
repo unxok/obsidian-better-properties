c9eaa4e9e1c029efafdb675ab761f191c1a9753f
unxok <anderluke707@gmail.com>
2025-09-22 23:10:55 -0700

    fixed illegal return

--- getChangelogEntry.js
+++ getChangelogEntry.js
@@ -2,8 +2,8 @@ import { readFileSync } from "fs";
 
 const version = process.env.npm_package_version;
 const escaped = version.replaceAll(".", "\\.");
-const changelogContent = readFileSync("changelog.md");
+const changelogContent = readFileSync("changelog.md", "utf8");
 
 const regex = new RegExp("^##\\s*" + escaped + "\\b.*?(?=^##\\s|\\Z)", "gms");
-const [entry] = regex.exec(changelogContent);
-return entry ?? "";
+const entry = regex.exec(changelogContent)?.[0] ?? "";
+console.log(entry);
