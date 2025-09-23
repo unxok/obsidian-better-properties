1104b1977a1899ceab1d52daec3acf0a380f7998
unxok <anderluke707@gmail.com>
2025-09-22 23:31:27 -0700

    fixed formatting of changelog entry

--- getChangelogEntry.js
+++ getChangelogEntry.js
@@ -4,6 +4,9 @@ const version = JSON.parse(readFileSync("package.json", "utf8")).version;
 const escaped = version.replaceAll(".", "\\.");
 const changelogContent = readFileSync("changelog.md", "utf8");
 
-const regex = new RegExp("^##\\s*" + escaped + "\\b.*?(?=^##\\s|\\Z)", "gms");
-const entry = regex.exec(changelogContent)?.[0] ?? "No changelog entry found";
-console.log(entry);
+const regex = new RegExp(
+	"^##\\s*" + escaped + "\\b[\\r\\n]+([\\s\\S]*?)(?=^##\\s|\\Z)",
+	"gms"
+);
+const entry = regex.exec(changelogContent)?.[1] ?? "No changelog entry found";
+console.log("# Changelog\n" + entry);
