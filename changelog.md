9b8f362b6d38041518d98ff2a02d7643912cf3e6
unxok <anderluke707@gmail.com>
2025-09-22 23:19:30 -0700

    fixed getting package version

--- changelog.md
+++ changelog.md
@@ -2,12 +2,12 @@
 
 Each release should include an entry detailing the changes made since the last release. Some specific details may be omitted for brevity.
 
-## 0.1.6
+## 0.1.7
 
 - test 1
 - test 2
 
-## 0.1.5
+## 0.1.6
 
 - should not get picked up
 - laskdjf
--- getChangelogEntry.js
+++ getChangelogEntry.js
@@ -1,9 +1,9 @@
 import { readFileSync } from "fs";
 
-const version = process.env.npm_package_version;
+const version = JSON.parse(readFileSync("package.json", "utf8")).version;
 const escaped = version.replaceAll(".", "\\.");
 const changelogContent = readFileSync("changelog.md", "utf8");
 
 const regex = new RegExp("^##\\s*" + escaped + "\\b.*?(?=^##\\s|\\Z)", "gms");
-const entry = regex.exec(changelogContent)?.[0] ?? "";
+const entry = regex.exec(changelogContent)?.[0] ?? "No changelog entry found";
 console.log(entry);
