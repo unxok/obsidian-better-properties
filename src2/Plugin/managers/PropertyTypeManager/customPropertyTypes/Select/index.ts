4b02b0a87fa974429f6426cc22c0d317218d326c
Unxok <me@unxok.com>
2026-04-18 16:35:48 -0700

    separated out shared css

--- src2/Plugin/managers/PropertyTypeManager/customPropertyTypes/Select/index.ts
+++ src2/Plugin/managers/PropertyTypeManager/customPropertyTypes/Select/index.ts
@@ -3,6 +3,7 @@ import renderSettings from "./renderSettings";
 import settingsSchema from "./settingsSchema";
 import { CustomPropertyType } from "../../types";
 import "./index.css";
+import "./utils/shared.css";
 
 // so eslint doesn't warn for unused import
 void settingsSchema;
